c48d73019859e67723c5770ff047669a87ee63d3
Kot_Barbarossa <kot_barba@MacBook-Pro-Kot-Barbarossa.local>
2023-01-08 13:30:42 +0300

    corrections after the third review isort

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,7 +1,8 @@
 from django_filters.rest_framework import FilterSet, filters
+from rest_framework.filters import SearchFilter
+
 from ingredients.models import Ingredient
 from recipes.models import Recipe
-from rest_framework.filters import SearchFilter
 from tags.models import Tag
 
 
